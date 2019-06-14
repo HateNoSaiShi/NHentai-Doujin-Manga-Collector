2e410b96911e4fcb55b7d110c5ef088374f2fead
藤原妹红 <38396479+HateNoSaiShi@users.noreply.github.com>
2019-06-15 02:51:00 +0800

    Add files via upload

---
+++ GetNHentai.py
@@ -0,0 +1,118 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 13 00:05:25 2018
+
+@author: Soseisei
+"""
+
+from urllib import request 
+from bs4 import BeautifulSoup as bs
+import os
+import time
+import shutil
+import re
+import requests
+
+def ValidateTitle(title):
+    rstr = r"[\/\\\:\*\?\"\<\>\|]"  # '/ \ : * ? " < > |'
+    new_title = re.sub(rstr, "", title) 
+    return new_title
+
+#===
+    
+def GetBeautifulSoup(gallery_id):
+    url = "https://nhentai.net/g/" + str(gallery_id)
+    headers = {
+        'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36'
+              } 
+    ##request_object = request.Request(url = url, headers = headers)
+    ##res = request.urlopen(request_object).read().decode('utf-8')
+    res = requests.get(url, headers = headers).text
+    html = bs(res, 'lxml')
+    return html
+    
+#===
+
+def GetAttributes(html):
+    title = html.find('h2').text
+    illustration_list = html.find_all(class_ = 'thumb-container')
+    num_of_illus = len(illustration_list)
+    temp_src = illustration_list[0].find(class_ = 'lazyload').attrs['data-src']
+    download_model_id = temp_src.split('galleries/')[1].split('/')[0]
+    res_dic = {
+                  'title': title,
+                  'illustration_num': num_of_illus,
+                  'download_model_id': download_model_id
+              }
+    return res_dic
+    
+#===
+
+def CreateFolder(folder_name, path = r'E://touhou//doujin//待归档爬虫//'):
+    to_create_path = path + folder_name
+    folder = os.path.exists(to_create_path)
+    if not folder:
+        os.makedirs(to_create_path)          
+    else:
+        shutil.rmtree(to_create_path)
+        os.makedirs(to_create_path)
+    return to_create_path
+
+#===
+
+def DownloadQuest(gallery_id, path = r'E://touhou//doujin//待归档爬虫//'):
+    html = GetBeautifulSoup(gallery_id)
+    attr_dic = GetAttributes(html)
+    folder_name = ValidateTitle(attr_dic['title'])
+    download_model_id = attr_dic['download_model_id']
+    num = attr_dic['illustration_num']
+    download_path = CreateFolder(folder_name, path)
+    download_url_prefix = 'https://i.nhentai.net/galleries/'
+    download_url_suffix1 = '.jpg'
+    download_url_suffix2 = '.png'
+    download_url_suffix3 = '.gif'
+    for i in range(num):
+        illus_number = i + 1
+        try:
+            download_url = download_url_prefix + download_model_id + '/' + str(illus_number) + download_url_suffix1
+            file_name = download_path + '//' + str(illus_number) + '.jpg'
+            #request.urlretrieve(download_url, filename=file_name, reporthook=None, data=None)
+            temp_html = requests.get(download_url)
+            with open(file_name, 'wb') as file:
+                file.write(temp_html.content)
+        except:
+            try:
+                download_url = download_url_prefix + download_model_id + '/' + str(illus_number) + download_url_suffix2
+                file_name = download_path + '//' + str(illus_number) + '.png'
+                #request.urlretrieve(download_url, filename=file_name, reporthook=None, data=None)
+                temp_html = requests.get(download_url)
+                with open(file_name, 'wb') as file:
+                    file.write(temp_html.content)
+            except:
+                download_url = download_url_prefix + download_model_id + '/' + str(illus_number) + download_url_suffix3
+                file_name = download_path + '//' + str(illus_number) + '.gif'
+                #request.urlretrieve(download_url, filename=file_name, reporthook=None, data=None)
+                temp_html = requests.get(download_url)
+                with open(file_name, 'wb') as file:
+                    file.write(temp_html.content)
+    print('complete ' + folder_name + str(gallery_id))
+    print('')
+    return 0
+    
+    
+#====
+if __name__ == '__main__':
+    #a = GetBeautifulSoup(232296)
+    #b = GetAttributes(a)
+    #CreateFolder('test')
+    _list= [182108]
+
+    for id in _list:
+        is_unfinished = 1
+        while (is_unfinished):
+            try:
+                is_unfinished = DownloadQuest(id)
+            except:
+                print('Error')
+                time.sleep(5)
+                
---
+++ NHentaiArchive.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 14 00:18:34 2018
+
+@author: Soseisei
+"""
+
+import os
+import shutil
+
+
+
+def GetAuthor(title):
+    author = title.split('[')[1].split(']')[0]
+    return author
+
+#===
+    
+def GetAllComicTitle(path = r'E://touhou//doujin//待归档爬虫//'):
+    return os.listdir(path)
+
+#===
+    
+def CreateFolder(folder_name, path = r'E://touhou//doujin//红字本//'):
+    to_create_path = path + folder_name
+    folder = os.path.exists(to_create_path)
+    if not folder:
+        os.makedirs(to_create_path)          
+    
+#===
+
+def MoveComic(old_path, new_path):
+    shutil.copytree(old_path, new_path)
+    
+#===
+
+def main():
+    old_path_prefix = r'E://touhou//doujin//待归档爬虫//'
+    new_path_prefix = r'E://touhou//doujin//红字本//'
+    comic_title_list = GetAllComicTitle()
+    for title in comic_title_list:
+        author = GetAuthor(title)
+        CreateFolder(author)
+        temp_old_path = old_path_prefix + title
+        temp_new_path = new_path_prefix + author + '//' + title
+        try:
+            MoveComic(temp_old_path, temp_new_path)
+        except:
+            print(title, 'exists!')
+        
+        
+        
+#=====
+if __name__ == '__main__':
+    main()
